b43ae42b5f913cd9fbbfcb99287bf9fec4984572
nateraw <nxr9266@g.rit.edu>
2022-07-25 17:19:37 -0400

    :sparkles: add another snippet

--- app.py
+++ app.py
@@ -15,3 +15,15 @@ Now you can copy this into your {'notebook' if nb else 'terminal'} and run:
 {"! " if nb else ""}pip install git+https://github.com/{repo_owner}/{repo_name}{"@" + branch_name if branch_name.strip() != "" else ""}
 ```
 """)
+
+
+st.markdown('---')
+
+st.markdown("### Colab Badge from Link")
+
+colab_url = st.text_input("Colab URL")
+st.markdown(f"""
+```
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]({colab_url})
+```
+""")
